75697862c527f38966691d32e6de6cc478293763
Oussama <thos.elmouden.oussama@gmail.com>
2023-02-12 17:22:55 +0100

    commit de la nova funcinalitat

--- src/me/oussama/git_eclipse/E01_main.java
+++ src/me/oussama/git_eclipse/E01_main.java
@@ -3,7 +3,10 @@ package me.oussama.git_eclipse;
 public class E01_main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		asi se agrega la nova funcionalitat
+		asdf
+		asdfasdf
+		asdf
 
 	}
 
--- src/me/oussama/git_eclipse/E01_relok.java
+++ src/me/oussama/git_eclipse/E01_relok.java
@@ -1,5 +1,9 @@
 package me.oussama.git_eclipse;
 
 public class E01_relok {
+	asdfasdf
+	asdfasdf
+	asdf
+	asdfa
 
 }
